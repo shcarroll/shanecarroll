f60c4f8fe73a77fad4cb4e95551acf1241f296e0
Shane Carroll <scarroll@poppulo.com>
2019-12-26 15:37:24 +0000

    Updated contact form with hidden bot-field

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -64,6 +64,15 @@ const Contact = props => {
               data-netlify="true"
               data-netlify-honeypot="bot-field"
             >
+              <FormItem label="Bot-field">
+                {getFieldDecorator("name", {
+                  rules: [
+                    {
+                      type: "hidden"
+                    }
+                  ]
+                })(<Input name="bot-field" />)}
+              </FormItem>
               <FormItem label="Name">
                 {getFieldDecorator("name", {
                   rules: [
